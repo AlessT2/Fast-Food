aa5946cbc7ba842506222615b91032920b4b519a
Lesther Villatoro <rodnyvillatorotzoc@gmail.com>
2023-11-07 18:47:46 -0600

    errores
    se corrigieron errores y se mejoró el menú

--- facturación y pagos.py
+++ facturación y pagos.py
@@ -11,13 +11,17 @@ def crear_factura(datos, encabezado, cadena):
 
 while True:
     print("///Escoja su tipo de pago///")
-    menu = input("1. Efectivo."
-                 "\n2. Tarjeta de crédito."
-                 "\nSeleccione su opción: ")
-    while menu == 1:
-        print("Haz pagado", cantidad)
-    while menu == 2:
-        print("Se ha descontado ", cantidad, "de su cuenta.")
-    continuar = input("¿Desea continuar en este menú?(S/N): ").upper()
-    if continuar.lower() != "s":
-        break
+    print("1. Efectivo. \n2. Tarjeta de crédito.")
+    try:
+        menu = int(input("Seleccione su opción: "))
+        while menu == 1:
+            print("Haz pagado", "cantidad")
+            break
+        while menu == 2:
+            print("Se ha descontado ", "cantidad", "de su cuenta.")
+            break
+        continuar = input("¿Desea continuar en este menú?(S/N): ").upper()
+        if continuar.lower() != "s":
+            break
+    except ValueError:
+        print("Seleccione una opción correcta.")
