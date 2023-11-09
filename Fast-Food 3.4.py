6d1d1805440679354635b83e84c55041714e90a3
AlessT2 <actzununt@correo.url.edu.gt>
2023-11-09 00:23:05 -0600

    Update Fast-Food 3.4.py

--- Fast-Food 3.4.py
+++ Fast-Food 3.4.py
@@ -1,3 +1,5 @@
+import queue
+order_queue = queue.Queue()
 vector_menus = []
 vector_drinks = []
 vector_order = []
@@ -32,16 +34,6 @@ class Menus:
         self.menu_price = int(input("Ingrese el precio del Menú: "))
         print("Menú registrado exitosamente")
 
-def mod_menu_name(menu_name):
-    print("Modificar nombre del Menú")
-    #while not vector_menus.empty():
-    for menu in vector_menus:
-        if menu.menu_name == menu_name:
-            menu.menu_name = input("Ingrese el nuevo nombre del menú: ")
-            return "El nuevo nombre del menú es: ",menu.menu_name
-    else:
-        return "El nombre del menú ingresado aún no ha sido registrado"
-
 
 class Drinks:
     def __init__(self, drink_name=0, drink_price=0):
@@ -54,6 +46,34 @@ class Drinks:
         self.drink_price = int(input("ingrese el precio de la bebida: "))
         print("La bebida se registro exitosamente")
 
+def mod_menu_name(menu_name):
+    print("Modificar precio del Menú")
+    #while not vector_menus.empty():
+    for menu in vector_menus:
+        if menu.menu_name == menu_name:
+            menu.menu_price = int(input("Ingrese el nuevo precio del menú: "))
+            return "El nuevo precio del menú es: ",menu.menu_name
+    else:
+        return "El nombre del menú ingresado aún no ha sido registrado"
+
+def add_order_queue():
+    print("Acciones de pedidos")
+    print("1. Pedidos pendientes")
+    print("2. Pedido en preparación")
+    print("3. Marcar pedido más antiguo listo para servir")
+    try:
+        #first_order = order_queue.get()
+        order_actions = int(input("Ingrese que acción desea realizar: "))
+        if order_actions == 1:
+            print("Los pedidos pendientes son: ",len(order_queue.queue))
+        elif order_actions == 2:
+            #preparing_order = order_queue.get()
+            print("El pedido que se está preparando es: ",order_queue.get())
+        elif order_actions == 3:
+            print("El pedido que está listo para servir es: ",order_queue.get())
+
+    except ValueError:
+        print("Por favor ingrese una acción válida entre 1 y 3")
 
 while True:
     print("Roles")
@@ -67,21 +87,21 @@ while True:
             print("2. Agregar Bebidas")
             print("3. Modificar nombre del menus")
             print("4. Modificar precio del menu")
+            print("5. Pedidos")
+
             try:
                 employee_actions = int(input("Ingrese que acción desea realizar: "))
+                menu = Menus()
                 if employee_actions == 1:
-                    menu = Menus()
-                    vector_menus.append(menu.menu_entry())
-                    #menu.menu_entry()
-                    #vector_menus.append(menu)
+                    menu.menu_entry()
+                    vector_menus.append(menu)
                 elif employee_actions == 2:
                     drink = Drinks()
                     vector_drinks.append(drink.drink_entry())
                     #drink.drink_entry()
                     #vector_drinks.append(drink)
                 elif employee_actions == 3:
-                    menu_name = input("Ingrese el nombre del menú que desea modificar: ")
-                    print(mod_menu_name(menu_name))
+                    menus.mod_menu_name()
                 else:
                     print("Por favor ingrese un valor válido de acciones para empleados")
             except ValueError:
@@ -89,6 +109,6 @@ while True:
         elif roles == 2:
             print("Acciones de clientes")
         else:
-            print("Por favor ingrese un valor entero entre 1 y 2 para seleccionar su Rol")
+            print("Por favor ingrese un valor entero entre 1 y 5 para seleccionar su Rol")
     except ValueError:
-        print("Por favor ingrese un valor entero entre 1 y 2 para seleccionar su Rol")
+        print("Por favor ingrese un valor entero entre 1 y 5 para seleccionar su Rol")
