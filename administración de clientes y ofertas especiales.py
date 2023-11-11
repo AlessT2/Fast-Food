276d8c54ca19c15525f9b8545f2a83a0800400cf
Lesther Villatoro <rodnyvillatorotzoc@gmail.com>
2023-11-10 19:34:44 -0600

    cambio en ofertas especiales
    se cambió la función de los descuentos ya que el código estaba en fase BETA y ahora funciona correctamente con el código completo

---
+++ administración de clientes y ofertas especiales.py
@@ -0,0 +1,54 @@
+import queue
+
+class Customer:
+    def __init__(self):
+        self.name = input("Nombre: ")
+        self.last_name = input("Apellido: ")
+        self.nit = input("NIT: ")
+        self.phone_number = input("Teléfono: ")
+        self.e_mail = input("Correo electrónico: ")
+
+class CustomersQueue:
+    def __init__(self):
+        self.customers = queue.Queue()
+
+    def add_customer(self):
+        customer = Customer()
+        self.customers.put(customer)
+
+    def display_customers(self):
+        while not self.customers.empty():
+            customer = self.customers.get()
+            print("Nombre:", customer.name)
+            print("Apellido:", customer.last_name)
+            print("NIT:", customer.nit)
+            print("Teléfono:", customer.phone_number)
+            print("Correo electrónico:", customer.e_mail)
+            print("")
+
+    def verify_customer(self, nit):
+        for customer in self.customers.queue:
+            if customer.nit == nit:
+                print("El cliente está registrado.")
+                # Aquí se puede permitir que el cliente realice un pedido
+                return
+        print("El cliente no está registrado. Por favor, regístrese.")
+        # Aquí se puede solicitar al cliente que se registre
+
+# Crear una nueva cola de clientes
+customers_queue = CustomersQueue()
+
+# Agregar algunos clientes
+customers_queue.add_customer()
+
+# verificar cliente
+customers_queue.verify_customer("15")
+
+# Mostrar la cola de clientes
+customers_queue.display_customers()
+
+#ofertas especiales///////////////////////////////////////////////////////////////////////
+def apply_discount(price):
+    discount = 0.1  # Descuento del 10% (puedes ajustarlo según tus necesidades)
+    discounted_price = price * (1 - discount)
+    return discounted_price
